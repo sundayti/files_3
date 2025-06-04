c009ecac51ac0f45c78536a69a57c9fcdbc05cdb
sundayti <pankratovvlad1@gmail.com>
2025-06-05 01:47:53 +0300

    add

---
+++ FileAnalysisService.Infrastructure/Migrations/20250604224718_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileAnalysisService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "file_analyses",
+                columns: table => new
+                {
+                    file_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    image_location = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    paragraph_count = table.Column<int>(type: "integer", nullable: false),
+                    word_count = table.Column<int>(type: "integer", nullable: false),
+                    character_count = table.Column<int>(type: "integer", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_file_analyses", x => x.file_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "file_analyses");
+        }
+    }
+}
