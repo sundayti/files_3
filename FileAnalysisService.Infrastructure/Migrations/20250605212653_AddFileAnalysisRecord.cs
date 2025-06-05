62ac5f768668ca8fc4babe9bad7816d9ac22ef94
sundayti <pankratovvlad1@gmail.com>
2025-06-06 00:27:04 +0300

    add

---
+++ FileAnalysisService.Infrastructure/Migrations/20250605212653_AddFileAnalysisRecord.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileAnalysisService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFileAnalysisRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image_location",
+                table: "file_analyses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image_location",
+                table: "file_analyses",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
