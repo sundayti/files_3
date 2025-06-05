51bc18938d765a689a0032f18141d4483772142c
sundayti <pankratovvlad1@gmail.com>
2025-06-06 00:20:10 +0300

    add

--- FileAnalysisService.Domain/Entities/FileAnalysisRecord.cs
+++ FileAnalysisService.Domain/Entities/FileAnalysisRecord.cs
@@ -1,52 +1,60 @@
 using FileAnalysisService.Domain.ValueObjects;
 
-namespace FileAnalysisService.Domain.Entities;
-
-public sealed class FileAnalysisRecord
+namespace FileAnalysisService.Domain.Entities
 {
-    public FileId FileId { get; private set; }
-    public ImageLocation CloudImageLocation { get; private set; }
-
-    // Статистические поля:
-    public int ParagraphCount { get; private set; }
-    public int WordCount { get; private set; }
-    public int CharacterCount { get; private set; }
-
-    public DateTime CreatedAtUtc { get; private set; }
-
-    // Приватный конструктор, который инициализирует все поля:
-    private FileAnalysisRecord(
-        FileId fileId,
-        ImageLocation imageLocation,
-        int paragraphCount,
-        int wordCount,
-        int characterCount,
-        DateTime createdAtUtc)
+    public sealed class FileAnalysisRecord
     {
-        FileId = fileId;
-        CloudImageLocation = imageLocation;
-        ParagraphCount = paragraphCount;
-        WordCount = wordCount;
-        CharacterCount = characterCount;
-        CreatedAtUtc = createdAtUtc;
-    }
+        // Существующие свойства
+        public FileId FileId { get; private set; }
+        public ImageLocation CloudImageLocation { get; private set; }
+        public int ParagraphCount { get; private set; }
+        public int WordCount { get; private set; }
+        public int CharacterCount { get; private set; }
+        public DateTime CreatedAtUtc { get; private set; }
 
-    /// <summary>
-    /// Фабричный метод для создания новой записи анализа.
-    /// </summary>
-    public static FileAnalysisRecord CreateNew(
-        FileId fileId,
-        ImageLocation imageLocation,
-        int paragraphCount,
-        int wordCount,
-        int characterCount)
-    {
-        return new FileAnalysisRecord(
-            fileId,
-            imageLocation,
-            paragraphCount,
-            wordCount,
-            characterCount,
-            DateTime.UtcNow);
+        // 1) Добавляем параметрless-конструктор для EF Core
+        private FileAnalysisRecord()
+        {
+            // EF Core нуждается в таком конструкторе, чтобы
+            // при чтении из БД создать пустой объект, а затем заполнить поля напрямую.
+        }
+
+        // 2) Сохраняем «полный» приватный конструктор для фабричного метода
+        private FileAnalysisRecord(
+            FileId fileId,
+            ImageLocation imageLocation,
+            int paragraphCount,
+            int wordCount,
+            int characterCount,
+            DateTime createdAtUtc)
+        {
+            FileId = fileId;
+            CloudImageLocation = imageLocation;
+            ParagraphCount = paragraphCount;
+            WordCount = wordCount;
+            CharacterCount = characterCount;
+            CreatedAtUtc = createdAtUtc;
+        }
+
+        /// <summary>
+        /// Фабричный метод для создания новой записи анализа.
+        /// EF Core при чтении из БД использует приватный конструктор без параметров,
+        /// затем напрямую устанавливает свойства через reflection.
+        /// </summary>
+        public static FileAnalysisRecord CreateNew(
+            FileId fileId,
+            ImageLocation imageLocation,
+            int paragraphCount,
+            int wordCount,
+            int characterCount)
+        {
+            return new FileAnalysisRecord(
+                fileId,
+                imageLocation,
+                paragraphCount,
+                wordCount,
+                characterCount,
+                DateTime.UtcNow);
+        }
     }
 }
